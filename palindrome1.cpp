d8f86c4db47cf49657655777068735ab954a6915
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2025-02-23 00:19:28 +0530

    Update palindrome1.cpp

--- palindrome1.cpp
+++ palindrome1.cpp
@@ -1,17 +1,24 @@
 #include<iostream>
 using namespace std;
+
 int main() {
-    int n;
-    cin >> n;
-    int rn = 0;
-  int dup=n;
-    while(n > 0) { 
-        int ld = n % 10; 
-        rn = (rn * 10) + ld;
-        n = n / 10;
+    int n; 
+    cin >> n; // Read input number
+    int rn = 0; // Reverse number
+    int dup = n; // Store original number
+
+    // Loop to reverse the digits of n
+    while(n > 0) {
+        int ld = n % 10; // Get last digit
+        rn = (rn * 10) + ld; // Build reversed number
+        n = n / 10; // Remove last digit
     }
-    if(dup==rn)
-    cout << "true";
-  else cout<<"false";
+
+    // Check if the original number equals the reversed number
+    if(dup == rn) 
+        cout << "true"; // It's a palindrome
+    else 
+        cout << "false"; // It's not a palindrome
+    
     return 0; 
 }
